2cdc4a7817a279f12945a9ee5683d09acb288da6
Hossein Talebi <hossein68talebi@gmail.com>
2016-02-28 11:05:10 -0700

    Formats the files

--- Kitchen/www/js/app.js
+++ Kitchen/www/js/app.js
@@ -4,56 +4,57 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic',
-                          'kitchen.controllers.authentication',
-                          'firebase',
-                          'ngCordova'])
+  'kitchen.controllers.authentication',
+  'firebase',
+  'ngCordova'
+])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    $ionicPlatform.ready(function() {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-.config(function ($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('home', {
-      url: "/home",
-      cache: false,
-      templateUrl: "templates/home.html"
-    })
-    .state('signup', {
-      url: "/signup",
-      cache: false,
-      controller: 'SignupCtrl',
-      templateUrl: "templates/signup.html"
-    })
-  .state('login', {
-    url: "/login",
-    controller: 'LoginCtrl',
-    cache: false,
-    templateUrl: "templates/login.html"
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
   })
-  .state('loading', {
-    url: "/loading",
-    cache: false,
-    templateUrl: "templates/loading.html"
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: "/home",
+        cache: false,
+        templateUrl: "templates/home.html"
+      })
+      .state('signup', {
+        url: "/signup",
+        cache: false,
+        controller: 'SignupCtrl',
+        templateUrl: "templates/signup.html"
+      })
+      .state('login', {
+        url: "/login",
+        controller: 'LoginCtrl',
+        cache: false,
+        templateUrl: "templates/login.html"
+      })
+      .state('loading', {
+        url: "/loading",
+        cache: false,
+        templateUrl: "templates/loading.html"
+      })
+      .state('welcome', {
+        url: "/welcome",
+        cache: false,
+        templateUrl: "templates/welcomeTemp.html"
+      })
+      // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/home');
   })
-  .state('welcome', {
-    url: "/welcome",
-    cache: false,
-    templateUrl: "templates/welcomeTemp.html"
-  })
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/home');
-})
