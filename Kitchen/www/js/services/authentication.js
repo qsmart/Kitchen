dffdf570f272b5fe6b137e3fda67c5def4d40416
Hossein Talebi <hossein68talebi@gmail.com>
2016-03-11 23:33:05 -0700

    login after sign up, and also refactoring login

--- Kitchen/www/js/services/authentication.js
+++ Kitchen/www/js/services/authentication.js
@@ -3,74 +3,88 @@ app.factory('authentication', function($state) {
 	var authnticated = false;
 	var credential = null;
 	var authError = null;
+	login = function(form) {
+		if (form.$valid) {
+			var email = form.email.$modelValue;
+			var password = form.password.$modelValue;
+			loginWithEmail(email, password);
+			//TODO
+		} else {
+			authError = "invalid"
+			authnticated = false;
+			console.log("invalid");
+			//TODO
+		}
+
+	};
+	loginWithEmail = function(email, password) {
+		$state.go('loading');
+		var ref = new Firebase("https://kitchenapp.firebaseio.com");
+		ref.authWithPassword({
+			email: email,
+			password: password
+		}, function(error, authData) {
+			authError = error;
+			if (error) {
+				authnticated = false;
+				$state.go('login', {
+					'hasServerError': true,
+					'error': error.toString().replace('Error: ', '')
+				});
+				console.log("Login Failed!", error);
+			} else {
+				authnticated = true;
+				credential = authData;
+				$state.go('welcome');
+				console.log("Authenticated successfully with payload:", authData);
+			}
+		});
+	};
+	signup = function(form) {
+		if (form.$valid) {
+			$state.go('loading');
+			var ref = new Firebase("https://kitchenapp.firebaseio.com");
+			var firstName = form.firstname.$modelValue;
+			var lastName = form.lastname.$modelValue;
+			var email = form.email.$modelValue;
+			var password = form.password.$modelValue; 
+			ref.createUser({
+				email: email,
+				password: password
+			}, function(error, userData) {
+				authError = error;
+				if (error) {
+					authnticated = false;
+					$state.go('signup', {
+						'hasServerError': true,
+						'error': error.toString().replace('Error: ', '')
+					});
+					console.log("Error creating user:", error);
+				} else {
+					authnticated = true;
+					credential = userData;
+					/*ref.child('cooks/' + username).set({
+						firstName: firstName,
+						lastName: lastName,
+						email: email,
+					});*/
+					console.log("Successfully created user account with uid:", userData.uid);
+					loginWithEmail(email, password);
+				}
+			});
+			//console.log(signupForm);
+			//TODO
+		} else {
+			authnticated = false;
+			authError = "invalid"
+			console.log("invalid");
+			//TODO
+		}
+	};
 	return {
-		login: function(form) {
-			if (form.$valid) {
-				$state.go('loading');
-				var ref = new Firebase("https://kitchenapp.firebaseio.com");
-				ref.authWithPassword({
-					email: form.email.$modelValue,
-					password: form.password.$modelValue
-				}, function(error, authData) {
-					authError = error;
-					if (error) {
-						authnticated = false;
-						$state.go('login', {
-							'hasServerError': true,
-							'error': error.toString().replace('Error: ', '')
-						});
-						console.log("Login Failed!", error);
-					} else {
-						authnticated = true;
-						credential = authData;
-						$state.go('welcome');
-						console.log("Authenticated successfully with payload:", authData);
-					}
-				});
-				//TODO
-			} else {
-				authError = "invalid"
-				authnticated = false;
-				console.log("invalid");
-				//TODO
-			}
-
-		},
-		signup: function(form) {
-			if (form.$valid) {
-				$state.go('loading');
-				var ref = new Firebase("https://kitchenapp.firebaseio.com");
-
-				ref.createUser({
-					firstName: form.firstname.$modelValue,
-					lastName: form.lastname.$modelValue,
-					email: form.email.$modelValue,
-					password: form.password.$modelValue
-				}, function(error, userData) {
-					authError = error;
-					if (error) {
-						authnticated = false;
-						$state.go('signup', {
-							'hasServerError': true,
-							'error': error.toString().replace('Error: ', '')
-						});
-						console.log("Error creating user:", error);
-					} else {
-						authnticated = true;
-						credential = userData;
-						$state.go('welcome');
-						console.log("Successfully created user account with uid:", userData.uid);
-					}
-				});
-				console.log(signupForm);
-				//TODO
-			} else {
-				authnticated = false;
-				authError = "invalid"
-				console.log("invalid");
-				//TODO
-			}
-		},
+		login: login,
+		loginWithEmail: loginWithEmail,
+		signup: signup,
 		facebookSignup: function() {
 
 		},
